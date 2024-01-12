830f5521b5092f44df81558762d76bc365532f05
fagirton <fagirton2015@gmail.com>
2024-01-12 18:55:09 +0300

    icons added

--- text.js
+++ text.js
@@ -4,15 +4,39 @@ export const data = {
 (ну типо) Главный хейтер персоны 3 и ронпы <p>
 В основном рассказываю про JRPG (и не только) да про новые и очень не новые игры в целом <p>
 Имею ворох старых консолей и хороший ПК <p>
+PS3 <img src="./media/icons/consoles/ps3.png" class="console-small-icon"/>, 
+Wii <img src="./media/icons/consoles/wii.png" class="console-small-icon"/>, 
+3DS <img src="./media/icons/consoles/3ds.png" class="console-small-icon"/>, 
+PS Vita <img src="./media/icons/consoles/psvita.png" class="wide-console-small-icon"/>, 
+GBA SP <img src="./media/icons/consoles/gba.png" class="wide-console-small-icon"/>, 
+Switch <img src="./media/icons/consoles/switch.png" class="wide-console-small-icon"/><p>
 -Пишу много текста, иногда небольшие обзоры даже <p>
 -Осуждаю ронпу, Франкс и персону 3 <p>
 -Никаких цветастых флагов не поддерживаю и неуместные феменитивы осуждаю <p>
 В остальном я добрый и дружелюбный, набираю читателей, чтобы с ними общаться, читайте и мьючите что-ли
 `,
+    second: `Я фанат: <p>
+    - Megami Tensei <p>
+    - Ace Attorney<p>
+    - большей части игр Nintendo<p>
+    - Chainsaw man<p>
+    - TWEWY<p>
+    - Attack on Titan<p>
+    - игр Valve<p>
+    - Metal Gear <p>
+    - 
+    Мой ник конечно кликбейтный, однако я лишь оцениваю игры с моей точки зрения  на основе своего опыта.<p>
+Конечно, моё мнение может не совпадать с вашим и это нормально и как хейтер, я никому не запрещаю играть в что-то и никого не обижаю и не оскорбляю этим.`,
+    third: `
+    Контакты:<p>
+Discord <img src="./media/icons/discord.svg" class="small-icon"/> - Fagirton, Kuzunoha from Russia#7750 </p>
+PSN <img src="./media/icons/psn.png" class="small-icon"/> - Fagirton2018<p>
+Xbox Live <img src="./media/icons/xboxlive.svg" class="wide-small-icon"/> - hohlid19<p>
+3DS <img src="./media/icons/n3ds.png" class="small-icon"/> - 3068-1626-9742 <p>`,
   },
-  english:{
+  english: {
     about: "funny text goes here",
     second: "text is subject to change",
-    third: "i mean it's definetly gonna change"
-}
+    third: "i mean it's definetly gonna change",
+  },
 };
