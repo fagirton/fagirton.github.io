8f289d67c9f98ec2f2bccef52a77c918ef002aa2
fagirton <fagirton2015@gmail.com>
2024-02-03 08:57:01 +0300

    sync small changes

--- text.js
+++ text.js
@@ -4,27 +4,35 @@ export const data = {
 (ну типо) Главный хейтер персоны 3 и ронпы <p>
 В основном рассказываю про JRPG (и не только) да про новые и очень не новые игры в целом <p>
 Имею ворох старых консолей и хороший ПК <p>
-PS3 <img src="./media/icons/consoles/ps3.png" class="console-small-icon"/>, 
-Wii <img src="./media/icons/consoles/wii.png" class="console-small-icon"/>, 
-3DS <img src="./media/icons/consoles/3ds.png" class="console-small-icon"/>, 
-PS Vita <img src="./media/icons/consoles/psvita.png" class="wide-console-small-icon"/>, 
-GBA SP <img src="./media/icons/consoles/gba.png" class="wide-console-small-icon"/>, 
-Switch <img src="./media/icons/consoles/switch.png" class="wide-console-small-icon"/><p>
+<fieldset>
+  <legend>Мои консоли</legend>
+    <div class="field-row">PS3 <img src="./media/icons/consoles/ps3.png" class="console-small-icon"/> </div>
+    <div class="field-row">Wii <img src="./media/icons/consoles/wii.png" class="console-small-icon"/> </div>
+    <div class="field-row">3DS <img src="./media/icons/consoles/3ds.png" class="console-small-icon"/> </div>
+    <div class="field-row">PS Vita <img src="./media/icons/consoles/psvita.png" class="wide-console-small-icon"/> </div>
+    <div class="field-row">GBA SP <img src="./media/icons/consoles/gba.png" class="wide-console-small-icon"/> </div>
+    <div class="field-row">Switch <img src="./media/icons/consoles/switch.png" class="wide-console-small-icon"/> </div>
+</fieldset>
+<p>
 -Пишу много текста, иногда небольшие обзоры даже <p>
 -Осуждаю ронпу, Франкс и персону 3 <p>
 -Никаких цветастых флагов не поддерживаю и неуместные феменитивы осуждаю <p>
 В остальном я добрый и дружелюбный, набираю читателей, чтобы с ними общаться, читайте и мьючите что-ли
 `,
-    second: `Я фанат: <p>
-    - Megami Tensei <p>
-    - Ace Attorney<p>
-    - большей части игр Nintendo<p>
-    - Chainsaw man<p>
-    - TWEWY<p>
-    - Attack on Titan<p>
-    - игр Valve<p>
-    - Metal Gear <p>
-    - 
+    second: `
+    <ul class="tree-view">
+    Я фанат:
+    <ul>
+     <li>Megami Tensei</li>
+     <li>Ace Attorney</li>
+     <li>большей части игр Nintendo</li>
+     <li>Chainsaw man</li>
+     <li>TWEWY</li>
+     <li>Attack on Titan</li>
+     <li>игр Valve</li>
+     <li>Metal Gear</li>
+    </ul> 
+    </ul><p>
     Мой ник конечно кликбейтный, однако я лишь оцениваю игры с моей точки зрения  на основе своего опыта.<p>
 Конечно, моё мнение может не совпадать с вашим и это нормально и как хейтер, я никому не запрещаю играть в что-то и никого не обижаю и не оскорбляю этим.`,
     third: `
